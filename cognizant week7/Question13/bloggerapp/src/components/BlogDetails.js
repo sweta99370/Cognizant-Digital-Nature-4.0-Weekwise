1f999aec620accbb99a4151d95dafb3855d9f1c5
sweta99370 <swetasalini444@gmail.com>
2025-07-31 18:21:50 +0530

    Add cognizant week7 folder

--- cognizant week7/Question13/bloggerapp/src/components/BlogDetails.js
+++
@@ -1,9 +0,0 @@
-const BlogDetails = () => {
-  return (
-    <div>
-      <h2>✍️ Blog Details</h2>
-      <p>Explore engaging blogs written by experts.</p>
-    </div>
-  );
-};
-export default BlogDetails;
--- cognizant week7/Question13/bloggerapp/src/components/CourseDetails.js
+++
@@ -1,9 +0,0 @@
-const CourseDetails = () => {
-  return (
-    <div>
-      <h2>🎓 Course Details</h2>
-      <p>Browse through our available online courses and tutorials.</p>
-    </div>
-  );
-};
-export default CourseDetails;
